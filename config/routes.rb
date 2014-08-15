906d9cc61ff63d342cbe2f13b74aa905454efd17
Ted Maruyama <tedmaruyama@gmail.com>
2014-08-14 17:09:26 -0700

    took : out of root route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
-  root to: 'controller#method'
+  root 'controller#method'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
